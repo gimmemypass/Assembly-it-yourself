f7648e1635e686c2a305bcadb22f0d3ddfe59a54
danila.golovenko <d.golovenko@chillgaming.net>
2024-10-06 00:08:48 +0400

    upd

---
+++ Assets/Scripts/GameBootsrapper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using SelfishFramework.Core.StateMachine;
+using UnityEngine;
+
+public class GameBootsrapper : MonoBehaviour
+{
+    private StateMachine stateMachine;
+    private void Start()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Network/Server.cs
@@ -0,0 +1,42 @@
+﻿using LiteNetLib;
+
+namespace Server
+{
+    public class Server
+    {
+        private readonly EventBasedNetListener listener;
+        private readonly NetManager netManager;
+        private readonly int maxConnection;
+        private readonly string key;
+
+        public Server(int port, int maxConnection, string key)
+        {
+            listener = new();
+            netManager = new(listener);
+            listener.ConnectionRequestEvent += OnConnectionRequest;
+            listener.PeerConnectedEvent += OnPeerConnected;
+            netManager.Start(port);
+            this.maxConnection = maxConnection;
+            this.key = key;
+        }
+
+        public void Update() => netManager.PollEvents();
+        public void Shutdown() => netManager.Stop();
+
+        private void OnPeerConnected(NetPeer peer)
+        {
+             
+        }
+
+        private void OnConnectionRequest(ConnectionRequest request)
+        {
+            if (netManager.ConnectedPeersCount < maxConnection)
+            {
+                request.AcceptIfKey(key);
+                return;
+            } 
+            
+            request.Reject();
+        }
+    }
+}
